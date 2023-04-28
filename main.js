2b48b6b4c961d762c438e061fb57b5e883b57b0c
gokarna123-goku <agokarna444@gmail.com>
2023-04-28 19:56:37 +0545

    added required link for owl carousel

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-// 
+//
 
+// Owl Carousel
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: true,
+    nav: true,
+    responsiveClass:true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+});
 
 // End
