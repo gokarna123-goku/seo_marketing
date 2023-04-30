63ab4529cdcb77e3f4b87b0787054a2e0c833090
gokarna123-goku <agokarna444@gmail.com>
2023-04-30 13:24:29 +0545

    working on it

--- main.js
+++ main.js
@@ -26,6 +26,12 @@ $(document).ready(function () {
         items: 1,
       },
       600: {
+        items: 2,
+      },
+      700: {
+        items: 2,
+      },
+      800: {
         items: 3,
       },
       1000: {
